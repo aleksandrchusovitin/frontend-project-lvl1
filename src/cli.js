ba2c7a50300cbf8bb36b081787ea837a873e6db6
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-05-01 13:28:05 +0700

    updat

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const greetUser = () => {
+  const userName = readlineSync.question('May I have your name? ');
+
+  console.log(`Hello, ${userName}!`);
+};
+
+export { greetUser };
