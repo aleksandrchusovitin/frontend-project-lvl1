7bc53c56f8d66a85bc06c43513f9839355babf14
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-05-04 22:36:12 +0700

    add logic to game-calc.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-import playGameCalc from '../src/game-calc';
+import playGameCalc from '../src/game-calc.js';
 
+console.log('Welcome to the Brain Games!');
 playGameCalc();
--- src/game-calc.js
+++ src/game-calc.js
@@ -1,5 +1,54 @@
+import readlineSync from 'readline-sync';
+
 const playGameCalc = () => {
-  
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('What is the result of the expression?');
+
+  const checkUserAnswer = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
+
+  const operators = ['+', '-', '*'];
+  const minValue = 1;
+  const maxValue = 100;
+  const countQuestions = 3;
+
+  for (let i = 0; i < countQuestions; i += 1) {
+    const operand1 = Math.floor(minValue + Math.random() * (maxValue + 1 - minValue));
+    const operand2 = Math.floor(minValue + Math.random() * (maxValue + 1 - minValue));
+    const operator = operators[Math.floor(Math.random() * operators.length)];
+
+    let correctAnswer;
+    switch (operator) {
+      case '+':
+        correctAnswer = operand1 + operand2;
+        break;
+      case '-':
+        correctAnswer = operand1 - operand2;
+        break;
+      case '*':
+        correctAnswer = operand1 * operand2;
+        break;
+      default:
+        break;
+    }
+    correctAnswer = correctAnswer.toString();
+
+    const question = `${operand1} ${operator} ${operand2}`;
+    console.log(`Question: ${question}`);
+
+    const userAnswer = readlineSync.question('Your answer: ');
+    const resultStep = checkUserAnswer(userAnswer, correctAnswer);
+
+    if (!resultStep) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}`);
 };
 
 export default playGameCalc;
