2430c7a1f79821b40ffdb01009852cb0b52b8cb5
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-05-05 13:41:13 +0700

    add logic(game engine) to src/index.js

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+
+const startGameEngine = (rulesGame, questions, correctAnswers) => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log(rulesGame);
+
+  const checkUserAnswer = (userAnswer, correctAnswer) => userAnswer === correctAnswer;
+
+  const countQuestions = 3;
+
+  for (let i = 0; i < countQuestions; i += 1) {
+    const question = questions[i];
+    const correctAnswer = correctAnswers[i];
+    console.log(`Question: ${question}`);
+
+    const userAnswer = readlineSync.question('Your answer: ');
+    const resultStep = checkUserAnswer(userAnswer, correctAnswer);
+
+    if (!resultStep) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}`);
+};
+
+export default startGameEngine;
