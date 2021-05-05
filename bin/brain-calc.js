fb124e1eea31af09c475ce0b504dc98cb20c33a8
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-05-05 13:40:35 +0700

    update imports in brain-calc.js and brain-even.js

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import playGameCalc from '../src/game-calc.js';
+import playGameCalc from '../games/game-calc.js';
 
-console.log('Welcome to the Brain Games!');
 playGameCalc();
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import playGameEven from '../src/game-even.js';
+import playGameEven from '../games/game-even.js';
 
-console.log('Welcome to the Brain Games!');
 playGameEven();
