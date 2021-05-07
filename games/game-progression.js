d24159f8e7591b77e6542a36bc4b87da2f37e832
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-05-07 12:04:31 +0700

    add logic to games/game-progression.js

--- games/game-progression.js
+++ games/game-progression.js
@@ -1,12 +1,30 @@
 import { cons } from '@hexlet/pairs';
+import { getRandomNumber } from '../src/functions.js';
 import startGameEngine from '../src/index.js';
 
 const playGameProgression = () => {
   const rulesGame = 'What number is missing in the progression?';
 
   const getPairQuestionCorrectAnswer = () => {
-    const question = 0;
-    const correctAnswer = [];
+    const lengthProgression = getRandomNumber(5, 15);
+    const indexHiddenNumber = getRandomNumber(0, lengthProgression - 1);
+    let nextNumberProgression = getRandomNumber(2, 50);
+    const stepProgression = getRandomNumber(2, 8);
+    const progressionArr = [nextNumberProgression];
+
+    let correctAnswer;
+
+    for (let i = 0; i < lengthProgression; i += 1) {
+      nextNumberProgression += stepProgression;
+      if (i === indexHiddenNumber) {
+        progressionArr.push('..');
+        correctAnswer = nextNumberProgression.toString();
+      } else {
+        progressionArr.push(nextNumberProgression);
+      }
+    }
+
+    const question = progressionArr.join(' ');
 
     return cons(question, correctAnswer);
   };
