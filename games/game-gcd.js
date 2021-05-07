e1c7e9644d70a3558797c1ff8db5e86ab2b65dee
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-05-07 10:45:02 +0700

    add logic to games/game-gcd.js

--- games/game-gcd.js
+++ games/game-gcd.js
@@ -2,10 +2,23 @@ import { cons } from '@hexlet/pairs';
 import startGameEngine from '../src/index.js';
 
 const playGameGcd = () => {
-  const rulesGame = '';
+  const rulesGame = 'Find the greatest common divisor of given numbers.';
 
   const getPairQuestionCorrectAnswer = () => {
-    
+    const findGcd = (a, b) => {
+      if (!b) {
+        return a;
+      }
+      return findGcd(b, a % b);
+    };
+
+    const minValue = 1;
+    const maxValue = 100;
+    const number1 = Math.floor(minValue + Math.random() * (maxValue + 1 - minValue));
+    const number2 = Math.floor(minValue + Math.random() * (maxValue + 1 - minValue));
+    const question = `${number1} ${number2}`;
+
+    const correctAnswer = findGcd(number1, number2).toString();
 
     return cons(question, correctAnswer);
   };
