0acf3d1480f219fe1c8361d52a400ea9b6fd9aaa
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-05-10 20:31:47 +0700

    update src/games/game-calc.js add function getCorrectAnswer()

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -5,6 +5,24 @@ const rulesGame = 'What is the result of the expression?';
 
 const getPairQuestionCorrectAnswer = () => {
   const getRandomIndexArr = (arr) => Math.floor(Math.random() * arr.length);
+  const getCorrectAnswer = (operand1, operator, operand2) => {
+    let correctAnswer;
+    switch (operator) {
+      case '+':
+        correctAnswer = operand1 + operand2;
+        break;
+      case '-':
+        correctAnswer = operand1 - operand2;
+        break;
+      case '*':
+        correctAnswer = operand1 * operand2;
+        break;
+      default:
+        break;
+    }
+    return correctAnswer.toString();
+  };
+
   const operators = ['+', '-', '*'];
 
   const operand1 = getRandomNumber(1, 100);
@@ -13,22 +31,7 @@ const getPairQuestionCorrectAnswer = () => {
   const operator = operators[randomIndexArr];
 
   const question = `${operand1} ${operator} ${operand2}`;
-
-  let correctAnswer;
-  switch (operator) {
-    case '+':
-      correctAnswer = operand1 + operand2;
-      break;
-    case '-':
-      correctAnswer = operand1 - operand2;
-      break;
-    case '*':
-      correctAnswer = operand1 * operand2;
-      break;
-    default:
-      break;
-  }
-  correctAnswer = correctAnswer.toString();
+  const correctAnswer = getCorrectAnswer(operand1, operator, operand2);
 
   return [question, correctAnswer];
 };
