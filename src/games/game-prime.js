5b32c1e9c888f5720af9d2e97a16b594980eb70b
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-05-12 21:10:38 +0700

    update src/games/game-prime. Add check case: number < 2 in isPrime()

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -4,6 +4,10 @@ import startGameEngine from '../index.js';
 const rulesGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
       return false;
